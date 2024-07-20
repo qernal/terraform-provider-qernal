cd7b5ee9b8679b5e11d6629017b38fc9272a3296
s1ntaxe770r <hello@jubril.xyz>
2024-07-20 15:03:40 +0100

    fix: regorganize secrets

---
+++ examples/resources/qernal_secret/certificate/certificate.tf
@@ -0,0 +1,61 @@
+
+terraform {
+  required_providers {
+    qernal = {
+      source = "qernal/qernal"
+    }
+  }
+}
+
+
+
+variable "qernal_token" {}
+variable "project_id" {}
+
+
+variable "public_key" {
+  default = <<EOT
+-----BEGIN CERTIFICATE-----
+MIIDazCCAlOgAwIBAgIUJgLsHCx/VLs0m6NkVXTKgzRqAQMwDQYJKoZIhvcNAQEL
+BQAwRTELMAkGA1UEBhMCQVUxEzARBgNVBAgMClNvbWUtU3RhdGUxITAfBgNVBAoM
+GEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZDAeFw0yMzA3MTQxNTMwMzhaFw0yNDA3
+MTMxNTMwMzhaMEUxCzAJBgNVBAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEw
+HwYDVQQKDBhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGQwggEiMA0GCSqGSIb3DQEB
+AQUAA4IBDwAwggEKAoIBAQC5p0Hn9WuPHuKTGK2IpOInPbOmZ3hPNmcj6SWDTa4s
+...
+-----END CERTIFICATE-----
+EOT
+}
+
+variable "private_key" {
+  default = <<EOT
+-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC5p0Hn9WuPHuKT
+GK2IpOInPbOmZ3hPNmcj6SWDTa4sWfV2BHHWxKaJCqKP8wGjIK5Xzim7xBxp+HKS
+fWmUFiRKh7kjgXUtqX3WfaZLVcCLgOK3OLwT5pZ/DcUkDKv7+JcLWt1vFxJcSzCk
+...
+-----END PRIVATE KEY-----
+EOT
+}
+
+provider "qernal" {
+  token = var.qernal_token
+}
+
+
+resource "qernal_secret_certificate" "example" {
+  name              = "my-certificate"
+  project_id        = "project-123"
+  certificate       = var.public_key
+  certificate_value = var.private_key
+}
+
+# Output the created_at date of the secret certificate
+output "certificate_created_at" {
+  value = qernal_secret_certificate.example.date.created_at
+}
+
+# Output the revision number of the secret certificate
+output "certificate_revision" {
+  value = qernal_secret_certificate.example.revision
+}
---
+++ examples/resources/qernal_secret/registry/.terraform/providers/registry.terraform.io/qernal/qernal/0.0.3/darwin_arm64/README.md
@@ -0,0 +1,47 @@
+# Terraform Provider for Qernal
+
+The Terraform provider for Qernal allows you to manage resources via
+terraform. This provider is maintained by Qernal.
+
+## Quick Start
+
+<!-- TODO insert quick start docs -->
+
+## Provider Usage
+
+<!-- TODO insert provider usage docs -->
+
+## Developing the provider
+
+
+To test the provider locally, Terraform provides an easy way to specify dev ovverides. Begin retrieving your GOBIN using the `go env` command:
+
+```bash
+go env GOBIN
+```
+
+
+Next edit or create file in the following path `~/.terraformrc`
+
+```terraform
+provider_installation {
+
+  dev_overrides {
+      "qernal/qernal" = "<PATH>"
+  }
+
+  # For all other providers, install them directly from their origin provider
+  # registries as normal. If you omit this, Terraform will _only_ use
+  # the dev_overrides block, and so no other providers will be available.
+  direct {}
+}
+```
+Be sure to replace <PATH> with the output returned from `go env GOBIN`
+
+### Generating docs
+
+To generate the docs locally;
+
+```bash
+go generate ./
+```
