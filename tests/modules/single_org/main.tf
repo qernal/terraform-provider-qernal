c47b7ea2ac037e6b3aed5100d19b2f4b9562f3a8
s1ntaxe770r <hello@jubril.xyz>
2024-07-25 12:41:26 +0100

    fix: add output values

--- tests/modules/single_org/main.tf
+++ tests/modules/single_org/main.tf
@@ -1,3 +1,7 @@
 resource "qernal_organisation" "org" {
   name = var.org_name
 }
+
+output "organisation_name" {
+  value =  qernal_organisation.org.name
+}
--- tests/modules/single_project/main.tf
+++ tests/modules/single_project/main.tf
@@ -2,3 +2,7 @@ resource "qernal_project" "project" {
   name   = var.project_name
   org_id = var.org_id
 }
+
+output "project_name"  {
+    value = qernal_project.project.name 
+}
