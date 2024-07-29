cea49eecefc9673aeddedc173aac8d6d3cfb6149
Jubril Oyetunji <jubriloyetunji@Jubrils-MacBook-Pro.local>
2024-07-29 13:12:07 +0100

    refactor: move org test

--- tests/qernal_organisation_test.go
+++ tests/qernal_organisation_test.go
@@ -45,3 +45,44 @@ func TestValidOrg(t *testing.T) {
 	tfOrgName := terraform.Output(t, terraformOptions, "organisation_name")
 	assert.Equal(t, orgName, tfOrgName)
 }
+
+func TestOrganisationDataSource(t *testing.T) {
+	t.Parallel()
+
+	orgId, _, err := createOrg()
+	if err != nil {
+		t.Fatal("Failed to create org")
+	}
+
+	moduleName := "./modules/org_datasource"
+
+	// Copy provider.tf
+	defer os.Remove(fmt.Sprintf("%s/provider.tf", moduleName))
+	err = files.CopyFile("./modules/provider.tf", fmt.Sprintf("%s/provider.tf", moduleName))
+	if err != nil {
+		t.Fatal("failed to copy provider file")
+	}
+
+	defer func() {
+		if err := cleanupTerraformFiles(moduleName); err != nil {
+			t.Logf("Warning: Failed to clean up Terraform files: %v", err)
+		}
+	}()
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: moduleName,
+		Vars: map[string]interface{}{
+			"org_id": orgId,
+		},
+	})
+
+	defer deleteOrg(orgId)
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Validate output
+	tfOrgID := terraform.Output(t, terraformOptions, "organisation_id")
+	assert.Equal(t, orgId, tfOrgID)
+
+}
