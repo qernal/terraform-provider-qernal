471a184f63613e81f36abe6691209a2bd96d73bd
Jubril Oyetunji <cyberdev01@protonmail.com>
2024-07-30 22:31:52 +0100

    feat: registry secrets tests

--- tests/qernal_secret_test.go
+++ tests/qernal_secret_test.go
@@ -65,3 +65,58 @@ func TestEnvironmentSecret(t *testing.T) {
 	outputSecretValue := terraform.Output(t, terraformOptions, "secret_value")
 	assert.Equal(t, secretValue, outputSecretValue)
 }
+
+func TestRegistrySecret(t *testing.T) {
+	t.Parallel()
+
+	orgId, _, err := createOrg()
+	if err != nil {
+		t.Fatal("Failed to creat org")
+	}
+	projectId, _, err := createProj(orgId)
+	if err != nil {
+		t.Fatal("Failed to create project")
+	}
+
+	secretName := strings.ToUpper(randomSecretName())
+	registryUrl := "ghcr.io"
+	authToken := randomSecretName()
+
+	moduleName := "./modules/registry_secret"
+
+	// Copy provider.tf
+	defer os.Remove(fmt.Sprintf("%s/provider.tf", moduleName))
+	err = files.CopyFile("./modules/provider.tf", fmt.Sprintf("%s/provider.tf", moduleName))
+	if err != nil {
+		t.Fatal("failed to copy provider file")
+	}
+
+	defer func() {
+		if err := cleanupTerraformFiles(moduleName); err != nil {
+			t.Logf("Warning: Failed to clean up Terraform files: %v", err)
+		}
+	}()
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: moduleName,
+		Vars: map[string]interface{}{
+			"project_id":   projectId,
+			"auth_token":   authToken,
+			"registry_url": registryUrl,
+			"secret_name":  secretName,
+		},
+	})
+
+	defer deleteProj(projectId)
+	defer deleteOrg(orgId)
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Validate outputs
+	outputSecretName := terraform.Output(t, terraformOptions, "secret_name")
+	assert.Equal(t, secretName, outputSecretName)
+
+	outputSecretValue := terraform.Output(t, terraformOptions, "secret_value")
+	assert.Equal(t, registryUrl, outputSecretValue)
+
+}
